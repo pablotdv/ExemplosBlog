224d2e935062fb0d95505ef121803e72fb8e7ae2
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-05-13 12:16:33 -0300

    Criando um projeto MVC com donet CLI - parte 2

---
+++ 02-criando-um-projeto-com-o-dotnet-cli/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace ExemploBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
+    }
+}
